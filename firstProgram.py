56ff04709289db21db40bbd20c28c2648f5fb3b7
shrikasooram-design <shrika.sooram@gmail.com>
2026-08-03 18:06:11 -0600

    Create firstProgram.py

---
+++ firstProgram.py
@@ -0,0 +1,26 @@
+
+#Write a program that stores:
+
+#Your name
+#The day number
+#A wound area
+#A cell count
+#Whether the wound is healed (True or False)
+
+#Then print each value.
+
+day0 = 100
+day1 = 94
+day3 = 87
+name = "shrika"
+woundarea = "medium"
+cellcount = 1000010100100
+healed = "True"
+
+print (day0)
+print (day1)
+print (day3)
+print (name)
+print (woundarea)
+print (cellcount)
+print (healed)
