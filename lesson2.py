c63df42662752a17a8e38e08e0b20453378d82c1
shrikasooram-design <shrika.sooram@gmail.com>
2026-08-04 17:59:38 -0600

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,70 @@
+wound_area = 100
+print("Original would area = ",wound_area)
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 1")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 2")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 3")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 4")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 5")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 6")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 7")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 8")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 9")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 10")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 11")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 12")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 13")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 14")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 15")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 16")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 17")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 18")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 19")
+
+wound_area = wound_area - 5
+print("It shrinks to",wound_area,"in day 20")
+
+
+
+
+
+
+
+
