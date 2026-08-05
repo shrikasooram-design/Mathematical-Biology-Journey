ef2789a51aa20bab412223c554911436b7429309
shrikasooram-design <shrika.sooram@gmail.com>
2026-08-04 19:29:21 -0600

    Create ForLoopsLesson.py

---
+++ ForLoopsLesson.py
@@ -0,0 +1,21 @@
+for day in range(1,6):
+    print(day)
+
+print("See the difference")
+
+for day in range(5):
+    print(day)
+
+
+wound_area = 100
+shrink_rate = 5
+
+
+# Wound on day x shrinked to y
+# in place of x i want day inplace of y i want result
+
+for day in range(20):
+  wound_area = wound_area-5  
+  print("wound shrinked to ",wound_area," on day",day)
+  
+
